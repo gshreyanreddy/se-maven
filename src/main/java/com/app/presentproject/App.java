a514a3057a755a809851ce87bdbc9e1a20c92718
gshreyanreddy <gogurushreyanreddy@gmail.com>
2023-12-18 11:37:15 +0530

    Demo For Team

---
+++ src/main/java/com/app/presentproject/App.java
@@ -0,0 +1,13 @@
+package com.app.presentproject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
